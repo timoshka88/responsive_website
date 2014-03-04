3ed5d064bb3f8a1c5c550ef34a0368a6971ea469
Fatima Khalilova <fatimakhalilova@gmail.com>
2014-03-04 17:19:14 -0500

    Added a javascript file with various functions and some pictures which will be required for the functions to work

---
+++ index2.js
@@ -0,0 +1,42 @@
+window.onload= function(){
+ 
+  document.getElementById('travel').onmouseover = function() {
+  	travel.src = "for_js/barcelona.jpg"
+  }
+
+   document.getElementById('travel').onclick = function() {
+  	travel.src = "for_js/kotor.jpg"
+  }
+
+   document.getElementById('travel').onmouseout = function() {
+  	travel.src = "img/Greece_athens.JPG"
+  }
+
+
+  document.getElementById('author').onclick = function() {
+  	window.open("http://24hoursofhappy.com/","_blank")
+  }
+
+
+
+
+
+
+
+  
+
+
+
+  
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+} 
